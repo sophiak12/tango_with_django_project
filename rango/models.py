8d6ea105ceaf1aa992d0cd3d87d8ce2e1fbd8a9f
Sophia Karamichali <2570646k@student.gla.ac.uk>
2022-02-02 23:48:03 +0000

    Chapter 6

--- rango/models.py
+++ rango/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 class Category(models.Model):
     
@@ -7,8 +8,11 @@ class Category(models.Model):
     name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
-    
-    
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args,**kwargs)
     
 
     class Meta:
--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from rango.models import Category
+from rango.models import Page
+
 def index(request):
     # Construct a dictionary to pass to the template engine as its context.
+
+    #Query the database for a list of ALL categories currently stored.
+    #Order the categories by the number of likes in descending order.
+    #Retrieve the top 5 only - or all if less than 5.
+    #Place the list in our context_dict dictionary (with our boldmessage)
+    #that will be passed to the template engine
+    category_list =  Category.objects.order_by('-likes')[:5]
+    page_list =  Page.objects.order_by('-views')[:5]
+    context_dict = {}
     # Note the key boldmessage matches to {{ boldmessage }} in the template!
-    context_dict={'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    context_dict['boldmessage']='Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories']=category_list
+    context_dict['pages']=page_list
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use. 
@@ -14,3 +28,37 @@ def index(request):
 def about(request):
     context_dict={'boldmessage': 'Rango says here is the about page.'}
     return render(request, "rango/about.html", context=context_dict)
+
+
+def show_category(request,category_name_slug):
+    #create a context dictionary which we can pass
+    #to the template rendering engine
+    context_dict = {}
+
+    try:
+        #Can we find a category name slug with the give name?
+        #if we cant, then .get() method raises a DoesNotExist exception
+        #The .get() method returns one model instance or raises and exception
+        category = Category.objects.get(slug=category_name_slug)
+
+        #retrieve all of the associated pages.
+        #the filter() will return a list of page objects or an empty list
+        pages = Page.objects.filter(category=category)
+
+        #Adds our results list to the template context under name pages.
+        context_dict['pages'] = pages
+        #We also add the category objects from
+        #the database to the context dictionary
+        #We'll use this in the template to verify that the category exists.
+        context_dict['category'] = category
+        
+    except Category.DoesNotExist:
+        #we get here if we didnt find the specified category
+        #dont do anything -
+        #the template will display "no categort" message
+        context_dict['category']=None
+        context_dict['pages']=None
+
+    #Go render the response and return it to the client.
+    return render(request, 'rango/category.html', context=context_dict)
+        
